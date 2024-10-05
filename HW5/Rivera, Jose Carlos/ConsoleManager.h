80b1ffca24d21a153259fd83204371fde6e6f5a7
Jose Carlos Rivera <josecirivera1906@gmail.com>
2024-10-05 09:22:44 +0800

    Add files via upload

---
+++ HW5/Rivera, Jose Carlos/ConsoleManager.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include "AConsole.h"
+#include "ProcessConsole.h"
+
+const std::string MAIN_CONSOLE = "MAIN_CONSOLE";
+const std::string PROCESS_CONSOLE = "PROCESS_CONSOLE";
+const std::string MARQUEE_CONSOLE = "MARQUEE_CONSOLE";
+typedef std::string String;
+
+class ConsoleManager {
+public:
+	typedef std::unordered_map<String, std::shared_ptr<AConsole>> ConsoleTable;
+
+	static ConsoleManager* getInstance();
+
+	void createConsole(String screenName);
+	void createConsoleWOswitch(String screenName);
+	void switchToConsole(String screenName);
+	void showListOfConsoles();
+	void switchToConsoleFromProcess(String screenName);
+	
+	std::shared_ptr<ProcessConsole> getProcessConsole(String screenName);
+	std::vector<String> getProcessConsoleNames();
+	
+	void printStringList(std::vector<String>& stringList);
+	void printProcessConsolesInfo();
+
+	const ConsoleTable& getConsoleTable() const;
+
+	void colorFG(int nColor);
+	void resetColor();
+	void printColored(String str, int colorCode);
+
+	void displayNvidiaSmi();
+
+	void printSmth();
+
+	bool running();
+
+private:
+	ConsoleTable consoleTable;
+	std::shared_ptr<AConsole> currentConsole;
+	std::shared_ptr<AConsole> previousConsole;
+
+	ConsoleManager();
+	~ConsoleManager() = default;
+	ConsoleManager(ConsoleManager const&) {}
+	ConsoleManager& operator=(ConsoleManager const&) {};
+
+	static ConsoleManager* instance;
+
+	bool isRunning = true;
+};
